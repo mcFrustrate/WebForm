acf729edfba5e38411b659af20a868e50db79eb1
mcFrustrate <78915782+mcFrustrate@users.noreply.github.com>
2021-03-03 22:55:55 +0800

    view and validation updated

--- WebForm/Models/PersonModel.cs
+++ WebForm/Models/PersonModel.cs
@@ -8,21 +8,27 @@ namespace WebForm.Models
 {
     public class PersonModel
     {
-        [Required]
+
+        [RegularExpression(@"^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$", ErrorMessage = "Last Name Invalid"), Required, StringLength(20)]
         public string LastName { get; set; }
 
-        [Required]
+        [RegularExpression(@"^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$", ErrorMessage = "First Name Invalid"), Required, StringLength(20)]
         public string FirstName { get; set; }
-        [Required]
+
+
+        [RegularExpression(@"^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$", ErrorMessage = "Gender Invalid"), Required, StringLength(6)]
         public string Gender { get; set; }
+
         [StringLength(11)]
         public string ContactNo { get; set; }
         
-        [Required]
+        [RegularExpression("^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$", ErrorMessage = "Must be a valid email"), Required]
         public string Email { get; set; }
         [Required]
         public string UserName { get; set; }
+
         [Required]
+        [StringLength(255, ErrorMessage = "Must be between 5 and 255 characters", MinimumLength = 5)]
         public string Password { get; set; }
     }
 }
